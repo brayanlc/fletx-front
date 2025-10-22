27b6088e4a80d430f0ad304c5f866824ed3639be
Brayan Carabali <brayanlc82@gmail.com>
2025-10-22 15:04:30 -0500

    Conectar frontend con backend
    - Configurar rutas de Angular con RouterModule
    - Añadir HttpClient para comunicación con API
    - Configurar app.html con router-outlet
    - Establecer rutas: login, register, productos, formularios
    - Redirección por defecto a login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
+import { RegisterComponent } from './components/register/register';
+import { ProductosComponent } from './components/productos/productos';
+import { ProductoFormComponent } from './components/producto-form/producto-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'productos/nuevo', component: ProductoFormComponent },
+  { path: 'productos/editar/:id', component: ProductoFormComponent },
+  { path: '**', redirectTo: '/login' }
+];
